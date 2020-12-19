6f1f90d4656f71daab206836effd63b56336f325
noiz354 <noiz354@gmail.com>
2020-12-19 12:28:17 +0700

    add constraintlayout. add kotlinstdlib. add BUILD that works with custom_package.

---
+++ examples/android/utils/src/main/java/com/tokopedia/utils/User.kt
@@ -0,0 +1,12 @@
+package utils.src.main.java.com.tokopedia.utils
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class User (
+        @PrimaryKey val uid: Int,
+        @ColumnInfo(name = "first_name") val firstName: String?,
+        @ColumnInfo(name = "last_name") val lastName: String?
+)
